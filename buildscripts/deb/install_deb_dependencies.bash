fa90a3e9b4abc42b6e1b00344ddb5cf697d1337b
Jonas Hauquier <jonas.hauquier@makehuman.org>
2014-02-26 21:30:40 +0100

    Adapt debian build script to use build_prepare. It also makes use of build.conf in buildscripts folder.

--- buildscripts/deb/install_deb_dependencies.bash
+++ buildscripts/deb/install_deb_dependencies.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-apt-get install python python-numpy python-opengl python-pyside.qtopengl python-pyside.qtcore python-pyside.qtgui python-pyside.qtopengl python-qt4-gl
+apt-get install python python-numpy python-opengl python-qt4 python-qt4-gl
 
