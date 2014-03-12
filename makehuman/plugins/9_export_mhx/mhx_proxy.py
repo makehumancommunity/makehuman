6ca563d3f4b533a39299cf443f22b0b3b90df882
Thomas Larsson <Thomas Larsson@localhost>
2014-03-12 05:28:33 +0100

    MHX exporter: New and consistent naming scheme for exported objects. Name generation is now located in a single place.

--- makehuman/plugins/9_export_mhx/mhx_proxy.py
+++ makehuman/plugins/9_export_mhx/mhx_proxy.py
@@ -64,38 +64,30 @@ class Writer(mhx_writer.Writer):
     #
     #-------------------------------------------------------------------------------
 
-    def writeProxy(self, fp, proxy, layer):
-        fp.write("""
-    NoScale False ;
-    """)
+    def writeProxy(self, fp, pxy, layer):
+        fp.write("NoScale False ;\n")
 
-        # Proxy material
+        obj = pxy.getSeedMesh()
+        mat = obj.material
+        if pxy.type != 'Proxymeshes':
+            self.writeProxyMaterial(fp, mat, pxy)
 
-        obj = proxy.getSeedMesh()
-        if proxy.type != 'Proxymeshes':
-            matname = self.writeProxyMaterial(fp, obj, proxy)
+        pxyname = self.meshName(pxy)
+        coords = self.config.scale * (pxy.getCoords() - self.config.offset)
 
-        # Proxy mesh
-
-        proxyname = getProxyName(self, proxy)
         fp.write(
-            "Mesh %s %s \n" % (proxyname, proxyname) +
-            "  Verts\n")
+            "Mesh %s %s \n" % (pxyname, pxyname) +
+            "  Verts\n" +
+            "".join( ["  v %.4f %.4f %.4f ;\n" % (x,-z,y) for x,y,z in coords] ) +
+            "  end Verts\n")
 
-        coords = self.config.scale * (proxy.getCoords() - self.config.offset)
-        fp.write("".join( ["  v %.4f %.4f %.4f ;\n" % (x,-z,y) for x,y,z in coords] ))
+        fp.write(
+            "  Faces\n" +
+            "".join( ["    f %d %d %d %d ;\n" % tuple(fv) for fv in obj.fvert] ) +
+            "    ftall 0 1 ;\n" +
+            "  end Faces\n")
 
-        fp.write("""
-      end Verts
-      Faces
-    """)
-
-        fp.write("".join( ["    f %d %d %d %d ;\n" % tuple(fv) for fv in obj.fvert] ))
-        fp.write("    ftall 0 1 ;\n")
-
-        fp.write("  end Faces\n")
-
-        # Proxy layers
+        # UVs
 
         fp.write(
             "  MeshTextureFaceLayer %s\n" % "Texture" +
@@ -109,26 +101,22 @@ class Writer(mhx_writer.Writer):
 
         # Proxy vertex groups
 
-        self.meshWriter.writeVertexGroups(fp, proxy)
+        self.meshWriter.writeVertexGroups(fp, pxy)
 
-        if proxy.type == 'Proxymeshes':
-            fp.write("  Material %sSkin ;" % self.name)
-        elif proxy.material:
-            fp.write("  Material %s ;" % matname)
-
-
-        fp.write("""
-    end Mesh
-    """)
+        if pxy.type == 'Proxymeshes':
+            fp.write("  Material %s ;\n" % self.materialName("Skin"))
+        elif pxy.material:
+            fp.write("  Material %s ;\n" % self.materialName(mat.name, pxy))
 
         # Proxy object
 
         fp.write(
-            "Object %s MESH %s \n" % (proxyname, proxyname) +
+            "end Mesh\n\n" +
+            "Object %s MESH %s \n" % (pxyname, pxyname) +
             "  parent Refer Object %s ;\n" % self.name +
             "  hide False ;\n" +
             "  hide_render False ;\n")
-        if proxy.wire:
+        if pxy.wire:
             fp.write("  draw_type 'WIRE' ;\n")
 
         # Proxy layers
@@ -141,10 +129,11 @@ class Writer(mhx_writer.Writer):
                 fp.write("0 ")
         fp.write(";\n\n")
 
-        self.meshWriter.writeArmatureModifier(fp, proxy)
-        self.writeProxyModifiers(fp, proxy)
+        self.meshWriter.writeArmatureModifier(fp, pxy)
+        self.writeProxyModifiers(fp, pxy)
 
-        fp.write("""
+        fp.write(
+"""
       parent_type 'OBJECT' ;
       color Array 1.0 1.0 1.0 1.0  ;
       show_name True ;
@@ -153,21 +142,21 @@ class Writer(mhx_writer.Writer):
       lock_rotation Array 1 1 1 ;
       lock_scale Array 1 1 1  ;
       Property MhxProxy True ;
-   """)
+""")
         fp.write(
             '      Property MhxScale theScale*%.4f ;\n' % self.config.scale +
-            '      Property MhxProxyName "%s" ;\n' % proxy.name.replace(" ","_") +
-            '      Property MhxProxyUuid "%s" ;\n' % proxy.uuid +
-            '      Property MhxProxyFile "%s" ;\n' % proxy.file.replace("\\", "/").replace(" ","%20") +
-            '      Property MhxProxyType "%s" ;\n' % proxy.type +
+            '      Property MhxProxyName "%s" ;\n' % pxy.name.replace(" ","_") +
+            '      Property MhxProxyUuid "%s" ;\n' % pxy.uuid +
+            '      Property MhxProxyFile "%s" ;\n' % pxy.file.replace("\\", "/").replace(" ","%20") +
+            '      Property MhxProxyType "%s" ;\n' % pxy.type +
             '    end Object')
 
     #-------------------------------------------------------------------------------
     #
     #-------------------------------------------------------------------------------
 
-    def writeProxyModifiers(self, fp, proxy):
-        for mod in proxy.modifiers:
+    def writeProxyModifiers(self, fp, pxy):
+        for mod in pxy.modifiers:
             if mod[0] == 'subsurf':
                 fp.write(
                     "    Modifier SubSurf SUBSURF\n" +
@@ -178,7 +167,7 @@ class Writer(mhx_writer.Writer):
                 offset = mod[1]
                 fp.write(
                     "    Modifier ShrinkWrap SHRINKWRAP\n" +
-                    "      target Refer Object %sBody ;\n" % self.name +
+                    "      target Refer Object %s ;\n" % self.meshName() +
                     "      offset %.4f ;\n" % offset +
                     "      use_keep_above_surface True ;\n" +
                     "    end Modifier\n")
@@ -193,31 +182,21 @@ class Writer(mhx_writer.Writer):
         return
 
 
-    def writeProxyMaterial(self, fp, obj, proxy):
-        mat = obj.material
-
+    def writeProxyMaterial(self, fp, mat, pxy):
         if mat.diffuseTexture:
-            #mat.diffuseTexture = proxy.getActualTexture(self.human)
+            #mat.diffuseTexture = pxy.getActualTexture(self.human)
             alpha = 0
         else:
             alpha = 1
 
-        prefix = getProxyName(self, proxy)
-        matname = prefix + mat.name
-        texnames = self.matWriter.writeTextures(fp, mat, prefix)
+        texnames = self.matWriter.writeTextures(fp, mat, pxy)
 
         # Write materials
 
-        fp.write("Material %s \n" % matname)
+        fp.write("Material %s \n" % self.materialName(mat.name, pxy))
         self.matWriter.writeMTexes(fp, texnames, mat)
         self.matWriter.writeMaterialSettings(fp, mat, alpha)
 
         fp.write(
             "  Property MhxDriven True ;\n" +
             "end Material\n\n")
-
-        return matname
-
-
-def getProxyName(self, proxy):
-    return ("%s_%s" % (self.name, proxy.name)).replace(" ", "_")
