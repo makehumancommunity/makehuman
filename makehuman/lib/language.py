cfac885ef95b45ec32229d3d133e427370ec683c
Jonas Hauquier <jonas.hauquier@makehuman.org>
2014-03-18 17:09:10 +0100

    Use ordered set to store untranslated strings, to keep related strings together, making it easier for translators.

--- makehuman/lib/language.py
+++ makehuman/lib/language.py
@@ -8,7 +8,7 @@ Translation and localization module.
 
 **Product Home Page:** http://www.makehuman.org/
 
-**Code Home Page:**    https://bitbucket.org/MakeHuman/makehuman/
+**Code Home Page:**    http://code.google.com/p/makehuman/
 
 **Authors:**           Manuel Bastioni, Marc Flerackers, Glynn Clements
 
@@ -29,11 +29,13 @@ import log
 import json
 from getpath import getPath, getSysDataPath
 
+import collections
+
 class Language(object):
     def __init__(self):
         self.language = None
         self.languageStrings = None
-        self.missingStrings = set()
+        self.missingStrings = OrderedSet()
         self.rtl = False
 
     def setLanguage(self, lang):
@@ -53,8 +55,6 @@ class Language(object):
             self.rtl = self.languageStrings['__options__'].get('rtl', False)
             
     def getLanguageString(self, string):
-        if not string:
-            return string
         if self.languageStrings is None:
             return string
         result = self.languageStrings.get(string)
@@ -73,5 +73,72 @@ class Language(object):
         with open(path, 'wb') as f:
             for string in self.missingStrings:
                 f.write('"%s": "",\n' % string.encode('utf8'))
+
+
+class OrderedSet(collections.MutableSet):
+    """
+    Set that maintains insertion order.
+
+    This is a python recipe, as referenced in the official documentation
+    (http://docs.python.org/2/library/collections.html)
+    Source: http://code.activestate.com/recipes/576694/
+    """
+
+    def __init__(self, iterable=None):
+        self.end = end = [] 
+        end += [None, end, end]         # sentinel node for doubly linked list
+        self.map = {}                   # key --> [key, prev, next]
+        if iterable is not None:
+            self |= iterable
+
+    def __len__(self):
+        return len(self.map)
+
+    def __contains__(self, key):
+        return key in self.map
+
+    def add(self, key):
+        if key not in self.map:
+            end = self.end
+            curr = end[1]
+            curr[2] = end[1] = self.map[key] = [key, curr, end]
+
+    def discard(self, key):
+        if key in self.map:        
+            key, prev, next = self.map.pop(key)
+            prev[2] = next
+            next[1] = prev
+
+    def __iter__(self):
+        end = self.end
+        curr = end[2]
+        while curr is not end:
+            yield curr[0]
+            curr = curr[2]
+
+    def __reversed__(self):
+        end = self.end
+        curr = end[1]
+        while curr is not end:
+            yield curr[0]
+            curr = curr[1]
+
+    def pop(self, last=True):
+        if not self:
+            raise KeyError('set is empty')
+        key = self.end[1][0] if last else self.end[2][0]
+        self.discard(key)
+        return key
+
+    def __repr__(self):
+        if not self:
+            return '%s()' % (self.__class__.__name__,)
+        return '%s(%r)' % (self.__class__.__name__, list(self))
+
+    def __eq__(self, other):
+        if isinstance(other, OrderedSet):
+            return len(self) == len(other) and list(self) == list(other)
+        return set(self) == set(other)
+
 
 language = Language()
