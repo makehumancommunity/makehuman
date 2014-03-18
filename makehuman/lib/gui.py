e30a19bc6137aa5b12ed9550f0b5158755563dd3
dochell <dochell@localhost>
2014-03-18 23:34:50 +0100

    Updating MakeHuman license.

---
+++ makehuman/lib/gui.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+"""
+**Project Name:**      MakeHuman
+
+**Product Home Page:** http://www.makehuman.org/
+
+**Code Home Page:**    https://bitbucket.org/MakeHuman/makehuman/
+
+**Authors:**           Glynn Clements
+
+**Copyright(c):**      MakeHuman Team 2001-2014
+
+**Licensing:**         AGPL3 (see also http://www.makehuman.org/node/318)
+
+**Coding Standards:**  See http://www.makehuman.org/node/165
+
+Abstract
+--------
+
+Alias from gui to qtgui
+"""
+
+from qtgui import *
