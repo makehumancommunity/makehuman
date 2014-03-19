2528c3fb2f2cfb2eea006f423b192b93a0dca20a
Jonas Hauquier <duststorm@users.noreply.github.com>
2014-03-19 17:05:19 +0100

    Update licenses for maketarget standalone

--- maketarget-standalone/maketarget-gui.py
+++ maketarget-standalone/maketarget-gui.py
@@ -16,6 +16,21 @@ GUI for the commandline MakeTarget tool
 
 **Licensing:**         AGPL3 (see also http://www.makehuman.org/node/319)
 
+    This file is part of MakeHuman (www.makehuman.org).
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 **Coding Standards:**  See http://www.makehuman.org/node/165
 
 Abstract
