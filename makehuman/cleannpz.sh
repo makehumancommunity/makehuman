472be278d37985952e3f9749956075a266a3b3fa
Jonas Hauquier <jonas.hauquier@makehuman.org>
2014-03-31 18:34:17 +0200

    Cleanup script cleans up binary proxies too

--- makehuman/cleannpz.sh
+++ makehuman/cleannpz.sh
@@ -6,7 +6,13 @@
 
 find . -type f -iname \*.npz -exec rm -rf {} \;
 
+# And mhpxy files
+
+find . -type f -iname \*.mhpxy -exec rm -rf {} \;
+
 
 # And .bin files
 
 find . -type f -iname \*.bin -exec rm -rf {} \;
+
+
