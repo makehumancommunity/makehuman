454737a5556b4c420a31ec480f5b22c2ac0e20c7
Jonas Hauquier <jonas.hauquier@makehuman.org>
2014-04-06 17:43:59 +0200

    Add todo to ogre exporter

---
+++ makehuman/lib/core.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+""" 
+**Project Name:**      MakeHuman
+
+**Product Home Page:** http://www.makehuman.org/
+
+**Code Home Page:**    https://bitbucket.org/MakeHuman/makehuman/
+
+**Authors:**           Glynn Clements
+
+**Copyright(c):**      MakeHuman Team 2001-2014
+
+**Licensing:**         AGPL3 (http://www.makehuman.org/doc/node/the_makehuman_application.html)
+
+    This file is part of MakeHuman (www.makehuman.org).
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+**Coding Standards:**  See http://www.makehuman.org/node/165
+
+Abstract
+--------
+
+TODO
+"""
+
+class Globals(object):
+    def __init__(self):
+        self.app = None
+        self.args = {}
+        self.world = []
+        self.cameras = []
+        self.canvas = None
+        self.windowHeight = 600
+        self.windowWidth = 800
+        self.clearColor = (0.0, 0.0, 0.0, 0.0)
+
+G = Globals()
