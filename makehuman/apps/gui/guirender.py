b222fc11a3540fec2b41b8fbc4153f3e888406e7
Jonas Hauquier <duststorm@users.noreply.github.com>
2014-04-08 00:51:07 +0200

    Move material properties from mesh to object. Fixes #317. Changed shaderChanged flag to use modification time, so that it can be shared in one material for multiple meshes (object3d) and that each render object effectively gets its shader updated for multiple meshes sharing the same object.

---
+++ makehuman/apps/gui/guirender.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+"""
+**Project Name:**      MakeHuman
+
+**Product Home Page:** http://www.makehuman.org/
+
+**Code Home Page:**    https://bitbucket.org/MakeHuman/makehuman/
+
+**Authors:**           Thanasis Papoutsidakis
+
+**Copyright(c):**      MakeHuman Team 2001-2014
+
+**Licensing:**         AGPL3 (http://www.makehuman.org/doc/node/the_makehuman_application.html)
+
+    This file is part of MakeHuman (www.makehuman.org).
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+**Coding Standards:**  See http://www.makehuman.org/node/165
+
+Abstract
+--------
+
+This module contains the Render Task View class to serve as a base class
+for task views that implement renderers and rendering related tasks.
+"""
+
+from core import G
+from guipose import PoseModeTaskView
+
+
+class RenderTaskView(PoseModeTaskView):
+    def __init__(self, category, name, label=None):
+        PoseModeTaskView.__init__(self, category, name, label)
+
+        self.oldShader = None
+        self.taskViewShader = None
+
+    # Render task views enable pose mode when shown, so that the selected pose
+    # is seen and rendered, and a light-based shader (default None) is
+    # selected, so that the actual scene lighting is simulated.
+
+    def onShow(self, event):
+        PoseModeTaskView.onShow(self, event)
+        import getpath
+
+        human = G.app.selectedHuman
+        self.oldShader = human.material.shader
+        human.material.shader = getpath.getSysDataPath(self.taskViewShader)
+
+    def onHide(self, event):
+        human = G.app.selectedHuman
+        human.material.shader = self.oldShader
+
+        PoseModeTaskView.onHide(self, event)
+
+    # renderingWidth, renderingHeight: properties for getting/setting
+    # the rendering width and height stored in the settings.
+
+    def getRenderingWidth(self):
+        return G.app.settings.get('rendering_width', 800)
+
+    def setRenderingWidth(self, value=None):
+        G.app.settings['rendering_width'] = 0 if not value else int(value)
+
+    renderingWidth = property(getRenderingWidth, setRenderingWidth)
+
+    def getRenderingHeight(self):
+        return G.app.settings.get('rendering_height', 600)
+
+    def setRenderingHeight(self, value=None):
+        G.app.settings['rendering_height'] = 0 if not value else int(value)
+
+    renderingHeight = property(getRenderingHeight, setRenderingHeight)
---
+++ makehuman/lib/inifile.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+"""
+**Project Name:**      MakeHuman
+
+**Product Home Page:** http://www.makehuman.org/
+
+**Code Home Page:**    https://bitbucket.org/MakeHuman/makehuman/
+
+**Authors:**           Glynn Clements
+
+**Copyright(c):**      MakeHuman Team 2001-2014
+
+**Licensing:**         AGPL3 (http://www.makehuman.org/doc/node/the_makehuman_application.html)
+
+    This file is part of MakeHuman (www.makehuman.org).
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+**Coding Standards:**  See http://www.makehuman.org/node/165
+
+Abstract
+--------
+
+TODO
+"""
+
+import json
+
+def _u2s(value):
+    if isinstance(value, unicode):
+        return str(value)
+    elif isinstance(value, dict):
+        return dict([(str(key), _u2s(val)) for key, val in value.iteritems()])
+    elif isinstance(value, list):
+        return [_u2s(val) for val in value]
+    else:
+        return value
+
+def parseINI(s, replace = []):
+    try:
+        result = json.loads(s)
+    except ValueError:
+        for src, dst in replace + [("'",'"'), (": True",": true"), (": False",": false"), (": None",": null")]:
+            s = s.replace(src, dst)
+        result = json.loads(s)
+    return _u2s(result)
+
+def formatINI(d):
+    return json.dumps(d, indent=4, ensure_ascii=True, encoding='iso-8859-1') + '\n'
