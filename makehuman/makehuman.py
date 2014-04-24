445321fad5015fcaa777b2a63a7087e420846b81
Jonas Hauquier <duststorm@users.noreply.github.com>
2014-04-24 23:09:55 +0200

    Bump to version 1.0.1 to prepare for patch release

--- makehuman/makehuman.py
+++ makehuman/makehuman.py
@@ -46,7 +46,7 @@ import re
 import subprocess
 
 ## Version information #########################################################
-version = [1, 0, 0]                     # Major, minor and patch version number
+version = [1, 0, 1]                     # Major, minor and patch version number
 release = False                         # False for nightly
 versionSub = ""                         # Short version description
 meshVersion = "hm08"                    # Version identifier of the basemesh
