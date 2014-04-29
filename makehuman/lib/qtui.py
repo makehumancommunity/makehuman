54a4c822938be67782646b2ee4990d11edb3a69c
Thanasis Papoutsidakis <PwnzorBot4000@gmail.com>
2014-04-29 19:51:56 +0300

    Backed out changeset: 52cb2198588d

--- makehuman/lib/qtui.py
+++ makehuman/lib/qtui.py
@@ -529,7 +529,7 @@ class Frame(QtGui.QMainWindow):
         # Use caching for faster generation on successive calls
         if hasattr(self, '_windowStateFlagsCache') and \
             stateflags == self._windowStateFlagsCache:
-            return self._windowStateCache.copy()
+            return self._windowStateCache
         else:
             self._windowStateFlagsCache = stateflags
 
