b111088dc2a16d199edbc580f1d5c7e7a636623d
Jonas Hauquier <jonas.hauquier@makehuman.org>
2014-07-30 19:16:42 +0200

    Fix skeleton library save handler: add newline at end of savefile line

--- makehuman/plugins/3_libraries_skeleton/skeletonlibrary.py
+++ makehuman/plugins/3_libraries_skeleton/skeletonlibrary.py
@@ -489,7 +489,7 @@ class SkeletonLibrary(gui3d.TaskView):
     def saveHandler(self, human, file):
         if human.getSkeleton():
             rigFile = getpath.getRelativePath(self.selectedRig, self.paths)
-            file.write('skeleton %s ' % rigFile)
+            file.write('skeleton %s\n' % rigFile)
 
 
 
