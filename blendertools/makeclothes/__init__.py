a2aecf7e3f489cc4a2f93c9da891ebfbfffa4b47
Thomas Larsson <Thomas Larsson@localhost>
2014-08-10 10:59:28 +0200

    Changed version numbers to 1.1.0 for Blender tools.

--- blendertools/makeclothes/__init__.py
+++ blendertools/makeclothes/__init__.py
@@ -40,7 +40,7 @@ Utility for making clothes to MH characters.
 bl_info = {
     "name": "Make Clothes",
     "author": "Thomas Larsson",
-    "version": (1,0,1),
+    "version": (1,1,0),
     "blender": (2,7,1),
     "location": "View3D > Properties > Make MH clothes",
     "description": "Make clothes and UVs for MakeHuman characters",
