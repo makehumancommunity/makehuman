6b76d8abb9a18ec6abac4323e2f6068caf1c7fae
Jonas Hauquier <jonas.hauquier@makehuman.org>
2014-10-12 17:30:39 +0200

    Make more effort to keep splash screen raised above main window. Explicitly hide main window until loading is finished. This will require testing on all platforms.

--- makehuman/lib/qtgui.py
+++ makehuman/lib/qtgui.py
@@ -1383,7 +1383,8 @@ class FileEntryView(QtGui.QWidget, Widget):
 
 class SplashScreen(QtGui.QSplashScreen):
     def __init__(self, image, version=""):
-        super(SplashScreen, self).__init__(G.app.mainwin, getPixmap(image))
+        super(SplashScreen, self).__init__(G.app.mainwin, getPixmap(image), QtCore.Qt.WindowStaysOnTopHint)
+        self.setWindowModality(QtCore.Qt.ApplicationModal)
         self._stdout = sys.stdout
         self.messageRect = QtCore.QRect(354, 531, 432, 41)
         self.messageAlignment = QtCore.Qt.AlignLeft
