cbad4da2d734f760e1e5c2ec8187b299bf05c4ed
Jonas Hauquier <jonas.hauquier@makehuman.org>
2014-10-27 23:56:29 +0100

    Store version number conform to PEP 0396

--- makehuman/makehuman.py
+++ makehuman/makehuman.py
@@ -46,7 +46,7 @@ import re
 import subprocess
 
 ## Version information #########################################################
-version = [1, 1, 0]                     # Major, minor and patch version number
+__version__ = "1.1.0"                   # Major, minor and patch version number
 release = False                         # False for nightly
 versionSub = ""                         # Short version description
 meshVersion = "hm08"                    # Version identifier of the basemesh
@@ -82,7 +82,9 @@ def getVersion():
     """
     Comparable version as list of ints
     """
-    return version
+    return __version__.split('.')
+
+version = getVersion()  # For backward compat.
 
 def getVersionStr(verbose=True):
     """
