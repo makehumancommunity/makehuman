fa7ccf4b9b987b646ee09a069b4e5fbd50a6ff46
Jonas Hauquier <jonas.hauquier@makehuman.org>
2015-03-15 23:49:10 +0100

    Reset progress bar on failed event. Fixes #759

--- makehuman/core/events3d.py
+++ makehuman/core/events3d.py
@@ -212,6 +212,8 @@ class EventHandler(object):
                 method(event)
         except Exception, _:
             log.warning('Exception during event %s', eventType, exc_info=True)
+            if topLevel:
+                self.eventFailed()
         EventHandler._depth -= 1
         if topLevel:
             self._logger.debug('callEvent: done')
@@ -219,6 +221,11 @@ class EventHandler(object):
                 G.app.redraw()
             return True
         return False
+
+    def eventFailed(self):
+        # Reset progress
+        if G.app:
+            G.app.progress(1)
 
     def attachEvent(self, eventName, eventMethod):
         setattr(self, eventName, eventMethod)
