33ee8a04f0900e1c7a881269f09118c960923ef5
Jonas Hauquier <duststorm@users.noreply.github.com>
2015-05-12 22:11:26 +0200

    Allow visualizing planes in skeleton debug

--- makehuman/plugins/3_libraries_skeleton/debugtab.py
+++ makehuman/plugins/3_libraries_skeleton/debugtab.py
@@ -87,6 +87,20 @@ class SkeletonDebugLibrary(gui3d.TaskView):
                 self.axisObj.setVisibility(self.showAxisTggl.selected)
         self.showAxisTggl.setSelected(True)
 
+        self.showBonesTggl = self.displayBox.addWidget(gui.CheckBox("Show bones"))
+        @self.showBonesTggl.mhEvent
+        def onClicked(event):
+            if self.skelObj:
+                self.skelObj.setVisibility(self.showBonesTggl.selected)
+        self.showBonesTggl.setSelected(True)
+
+        self.showPlanesTggl = self.displayBox.addWidget(gui.CheckBox("Show bone planes"))
+        @self.showPlanesTggl.mhEvent
+        def onClicked(event):
+            if self.planesObj:
+                self.planesObj.setVisibility(self.showPlanesTggl.selected)
+        self.showPlanesTggl.setSelected(False)
+
     def _unloadSkeletonMesh(self):
         if self.skelObj:
             # Remove old skeleton mesh
@@ -115,6 +129,7 @@ class SkeletonDebugLibrary(gui3d.TaskView):
         self.skelObj.setShadeless(0)
         self.skelObj.setSolid(0)
         self.skelObj.setRotation(self.human.getRotation())
+        self.skelMesh.setVisibility(self.showBonesTggl.selected)
 
         self.axisMesh = skeleton_drawing.meshFromSkeleton(skel, "axis")
         self.axisMesh.name = self.axisMesh.name + '-axis-skeletonDebug'
@@ -125,8 +140,91 @@ class SkeletonDebugLibrary(gui3d.TaskView):
         self.axisObj.material.configureShading(vertexColors=True)
         self.axisObj.material.depthless = True
         self.axisObj.setRotation(self.human.getRotation())
+        self.axisObj.setVisibility(self.showAxisTggl.selected)
+
+        self.drawPlanes(skel)
 
         mh.redraw()
+
+    def drawPlanes(self, skel):
+        def _get_face_count(skel):
+            result = 0
+            for bone in skel.getBones():
+                if isinstance(bone.roll, list):
+                    result += len(bone.roll)
+                else:
+                    result += 1
+            return result
+
+        import module3d
+        self.planesMesh = module3d.Object3D("SkeletonPlanesMesh", 3)
+
+        facecount = _get_face_count(skel)
+        vertcount = 3*facecount
+        faces = np.arange(vertcount, dtype=np.uint16).reshape((facecount, 3))
+        verts = np.zeros((vertcount,3), dtype=np.float32)
+        vcolors = 255*np.ones((vertcount, 4), dtype=np.uint8)
+        PLANE_COLORS = [[60,230,200], [220, 60, 230], [230, 180, 60], [230, 90, 60], [60, 230, 60], [60, 120, 230]]
+
+        fgroups = np.zeros(len(faces), dtype=np.uint16)
+        v_offset = 0
+        for bIdx, bone in enumerate(skel.getBones()):
+            fg = self.planesMesh.createFaceGroup(bone.name)
+            fgroups[bIdx:bIdx+1] = np.repeat(np.array(fg.idx, dtype=np.uint16), 1)
+            if isinstance(bone.roll, list):
+                for p_idx, plane_name in enumerate(bone.roll):
+                    plane_name = bone.roll[0]
+                    plane_joints = bone.planes[plane_name]
+
+                    j1,j2,j3 = plane_joints
+                    in_rest = False
+                    p1 = skel.getJointPosition(j1, self.human, in_rest)[:3] * skel.scale
+                    p2 = skel.getJointPosition(j2, self.human, in_rest)[:3] * skel.scale
+                    p3 = skel.getJointPosition(j3, self.human, in_rest)[:3] * skel.scale
+                    verts[v_offset:v_offset+3] = [p1, p2, p3]
+                    vcolors[v_offset:v_offset+3,:3] = PLANE_COLORS[p_idx]
+                    fgroups[v_offset/3] = fg.idx
+                    v_offset += 3
+            elif isinstance(bone.roll, basestring):
+                plane_name = bone.roll
+                plane_joints = bone.planes[plane_name]
+
+                j1,j2,j3 = plane_joints
+                in_rest = False
+                p1 = skel.getJointPosition(j1, self.human, in_rest)[:3] * skel.scale
+                p2 = skel.getJointPosition(j2, self.human, in_rest)[:3] * skel.scale
+                p3 = skel.getJointPosition(j3, self.human, in_rest)[:3] * skel.scale
+                verts[v_offset:v_offset+3] = [p1, p2, p3]
+                vcolors[v_offset:v_offset+3,:3] = PLANE_COLORS[0]
+                fgroups[v_offset/3] = fg.idx
+                v_offset += 3
+            else:
+                p1 = p2 = p3 = [0.0, 0.0, 0.0]
+                verts[v_offset:v_offset+3] = [p1, p2, p3]
+                vcolors[v_offset:v_offset+3,:3] = [255, 0, 0]
+                fgroups[v_offset/3] = fg.idx
+                v_offset += 3
+
+        self.planesMesh.setCoords(verts)
+        self.planesMesh.setColor(vcolors)
+        self.planesMesh.setUVs(np.zeros((1, 2), dtype=np.float32)) # Set trivial UV coordinates
+        self.planesMesh.setFaces(faces, None, fgroups)
+        
+        self.planesMesh.updateIndexBuffer()
+        self.planesMesh.calcNormals()
+        self.planesMesh.update()
+
+        self.planesMesh.setCameraProjection(0)
+
+        self.planesMesh.priority = 100
+        self.planesMesh.setPickable(False)
+        self.planesObj = self.addObject(gui3d.Object(self.planesMesh, self.human.getPosition()) )
+        self.planesObj.setShadeless(0)
+        self.planesObj.setSolid(0)
+        self.planesObj.setRotation(self.human.getRotation())
+        self.planesObj.material.backfaceCull = False
+        self.planesObj.setVisibility(self.showPlanesTggl.selected)
+
 
     def reloadBoneExplorer(self):
         # Remove old radio buttons
@@ -140,7 +238,10 @@ class SkeletonDebugLibrary(gui3d.TaskView):
         if not skel:
             return
 
-        for bone in skel.getBones():
+        bones = [(bone.name, bone) for bone in skel.getBones()]
+        bones.sort()  # Sort by name
+        for _, bone in bones:
+            # TODO using a tree widget here, as in Utilities > Data would be good
             radioBtn = self.boneBox.addWidget(gui.RadioButton(self.boneSelector, bone.name))
             @radioBtn.mhEvent
             def onClicked(event):
