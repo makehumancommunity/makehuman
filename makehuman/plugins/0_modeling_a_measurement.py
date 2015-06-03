338f3f9190bd8cc3a621ffdced193fea8746ce1f
Jonas Hauquier <jonas.hauquier@makehuman.org>
2015-06-03 09:58:36 +0200

    #711: Update measurement modifier references

--- makehuman/plugins/0_modeling_a_measurement.py
+++ makehuman/plugins/0_modeling_a_measurement.py
@@ -196,16 +196,16 @@ class MeasureTaskView(guimodifier.ModifierTaskView):
 
         lang = language.language
         self.height.setTextFormat(lang.getLanguageString('Height') + ': %s', height)
-        self.chest.setTextFormat(lang.getLanguageString('Chest') + ': %s', self.getMeasure('measure/measure-bust-decrease|increase'))
-        self.waist.setTextFormat(lang.getLanguageString('Waist') + ': %s', self.getMeasure('measure/measure-waist-decrease|increase'))
-        self.hips.setTextFormat(lang.getLanguageString('Hips') + ': %s', self.getMeasure('measure/measure-hips-decrease|increase'))
+        self.chest.setTextFormat(lang.getLanguageString('Chest') + ': %s', self.getMeasure('measure/measure-bust-circ-decr|incr'))
+        self.waist.setTextFormat(lang.getLanguageString('Waist') + ': %s', self.getMeasure('measure/measure-waist-circ-decr|incr'))
+        self.hips.setTextFormat(lang.getLanguageString('Hips') + ': %s', self.getMeasure('measure/measure-hips-circ-decr|incr'))
 
     def syncBraSizes(self):
         # TODO unused
         human = G.app.selectedHuman
 
-        bust = self.ruler.getMeasure(human, 'measure/measure-bust-decrease|increase', 'metric')
-        underbust = self.ruler.getMeasure(human, 'measure/measure-underbust-decrease|increase', 'metric')
+        bust = self.ruler.getMeasure(human, 'measure/measure-bust-circ-decr|incr', 'metric')
+        underbust = self.ruler.getMeasure(human, 'measure/measure-underbust-circ-decr|incr', 'metric')
 
         eucups = ['AA', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K']
 
@@ -294,34 +294,34 @@ class Ruler:
         # TODO define in data file?
 
         self.Measures = {}
-        self.Measures['measure/measure-neckcirc-decrease|increase'] = [7514,10358,7631,7496,7488,7489,7474,7475,7531,7537,7543,7549,7555,7561,7743,7722,856,1030,1051,850,844,838,832,826,820,756,755,770,769,777,929,3690,804,800,808,801,799,803,7513,7515,7521,7514]
-        self.Measures['measure/measure-neckheight-decrease|increase'] = [853,854,855,856,857,858,1496,1491]
+        self.Measures['measure/measure-neck-circ-decr|incr'] = [7514,10358,7631,7496,7488,7489,7474,7475,7531,7537,7543,7549,7555,7561,7743,7722,856,1030,1051,850,844,838,832,826,820,756,755,770,769,777,929,3690,804,800,808,801,799,803,7513,7515,7521,7514]
+        self.Measures['measure/measure-neck-height-decr|incr'] = [853,854,855,856,857,858,1496,1491]
 
 
-        self.Measures['measure/measure-upperarm-decrease|increase']=[8383,8393,8392,8391,8390,8394,8395,8399,10455,10516,8396,8397,8398,8388,8387,8386,10431,8385,8384,8389]
-        self.Measures['measure/measure-upperarmlenght-decrease|increase'] = [8274,10037]
+        self.Measures['measure/measure-upperarm-circ-decr|incr']=[8383,8393,8392,8391,8390,8394,8395,8399,10455,10516,8396,8397,8398,8388,8387,8386,10431,8385,8384,8389]
+        self.Measures['measure/measure-upperarm-length-decr|incr'] = [8274,10037]
 
-        self.Measures['measure/measure-lowerarmlenght-decrease|increase'] = [10040,10548]
-        self.Measures['measure/measure-wrist-decrease|increase']=[10208,10211,10212,10216,10471,10533,10213,10214,10215,10205,10204,10203,10437,10202,10201,10206,10200,10210,10209,10208]
+        self.Measures['measure/measure-lowerarm-length-decr|incr'] = [10040,10548]
+        self.Measures['measure/measure-wrist-circ-decr|incr']=[10208,10211,10212,10216,10471,10533,10213,10214,10215,10205,10204,10203,10437,10202,10201,10206,10200,10210,10209,10208]
 
-        self.Measures['measure/measure-frontchest-decrease|increase']=[1437,8125]
-        self.Measures['measure/measure-bust-decrease|increase']=[8439,8455,8462,8446,8478,8494,8557,8510,8526,8542,10720,10601,10603,10602,10612,10611,10610,10613,10604,10605,10606,3942,3941,3940,3950,3947,3948,3949,3938,3939,3937,4065,1870,1854,1838,1885,1822,1806,1774,1790,1783,1767,1799,8471]
-        self.Measures['measure/measure-underbust-decrease|increase'] = [10750,10744,10724,10725,10748,10722,10640,10642,10641,10651,10650,10649,10652,10643,10644,10645,10646,10647,10648,3988,3987,3986,3985,3984,3983,3982,3992,3989,3990,3991,3980,3981,3979,4067,4098,4073,4072,4094,4100,4082,4088, 4088]
-        self.Measures['measure/measure-waist-decrease|increase'] = [4121,10760,10757,10777,10776,10779,10780,10778,10781,10771,10773,10772,10775,10774,10814,10834,10816,10817,10818,10819,10820,10821,4181,4180,4179,4178,4177,4176,4175,4196,4173,4131,4132,4129,4130,4128,4138,4135,4137,4136,4133,4134,4108,4113,4118,4121]
-        self.Measures['measure/measure-napetowaist-decrease|increase']=[1491,4181]
-        self.Measures['measure/measure-waisttohip-decrease|increase']=[4121,4341]
-        self.Measures['measure/measure-shoulder-decrease|increase'] = [7478,8274]
+        self.Measures['measure/measure-frontchest-dist-decr|incr']=[1437,8125]
+        self.Measures['measure/measure-bust-circ-decr|incr']=[8439,8455,8462,8446,8478,8494,8557,8510,8526,8542,10720,10601,10603,10602,10612,10611,10610,10613,10604,10605,10606,3942,3941,3940,3950,3947,3948,3949,3938,3939,3937,4065,1870,1854,1838,1885,1822,1806,1774,1790,1783,1767,1799,8471]
+        self.Measures['measure/measure-underbust-circ-decr|incr'] = [10750,10744,10724,10725,10748,10722,10640,10642,10641,10651,10650,10649,10652,10643,10644,10645,10646,10647,10648,3988,3987,3986,3985,3984,3983,3982,3992,3989,3990,3991,3980,3981,3979,4067,4098,4073,4072,4094,4100,4082,4088, 4088]
+        self.Measures['measure/measure-waist-circ-decr|incr'] = [4121,10760,10757,10777,10776,10779,10780,10778,10781,10771,10773,10772,10775,10774,10814,10834,10816,10817,10818,10819,10820,10821,4181,4180,4179,4178,4177,4176,4175,4196,4173,4131,4132,4129,4130,4128,4138,4135,4137,4136,4133,4134,4108,4113,4118,4121]
+        self.Measures['measure/measure-napetowaist-dist-decr|incr']=[1491,4181]
+        self.Measures['measure/measure-waisttohip-dist-decr|incr']=[4121,4341]
+        self.Measures['measure/measure-shoulder-dist-decr|incr'] = [7478,8274]
 
-        self.Measures['measure/measure-hips-decrease|increase'] = [4341,10968,10969,10971,10970,10967,10928,10927,10925,10926,10923,10924,10868,10875,10861,10862,4228,4227,4226,4242,4234,4294,4293,4296,4295,4297,4298,4342,4345,4346,4344,4343,4361,4341]
+        self.Measures['measure/measure-hips-circ-decr|incr'] = [4341,10968,10969,10971,10970,10967,10928,10927,10925,10926,10923,10924,10868,10875,10861,10862,4228,4227,4226,4242,4234,4294,4293,4296,4295,4297,4298,4342,4345,4346,4344,4343,4361,4341]
 
-        self.Measures['measure/measure-upperlegheight-decrease|increase'] = [10970,11230]
-        self.Measures['measure/measure-thighcirc-decrease|increase'] = [11071,11080,11081,11086,11076,11077,11074,11075,11072,11073,11069,11070,11087,11085,11084,12994,11083,11082,11079,11071]
+        self.Measures['measure/measure-upperleg-height-decr|incr'] = [10970,11230]
+        self.Measures['measure/measure-thigh-circ-decr|incr'] = [11071,11080,11081,11086,11076,11077,11074,11075,11072,11073,11069,11070,11087,11085,11084,12994,11083,11082,11079,11071]
 
-        self.Measures['measure/measure-lowerlegheight-decrease|increase'] = [11225,12820]
-        self.Measures['measure/measure-calf-decrease|increase'] = [11339,11336,11353,11351,11350,13008,11349,11348,11345,11337,11344,11346,11347,11352,11342,11343,11340,11341,11338,11339]
+        self.Measures['measure/measure-lowerleg-height-decr|incr'] = [11225,12820]
+        self.Measures['measure/measure-calf-circ-decr|incr'] = [11339,11336,11353,11351,11350,13008,11349,11348,11345,11337,11344,11346,11347,11352,11342,11343,11340,11341,11338,11339]
 
-        self.Measures['measure/measure-ankle-decrease|increase'] = [11460,11464,11458,11459,11419,11418,12958,12965,12960,12963,12961,12962,12964,12927,13028,12957,11463,11461,11457,11460]
-        self.Measures['measure/measure-kneecirc-decrease|increase'] = [11223,11230,11232,11233,11238,11228,11229,11226,11227,11224,11225,11221,11222,11239,11237,11236,13002,11235,11234,11223]
+        self.Measures['measure/measure-ankle-circ-decr|incr'] = [11460,11464,11458,11459,11419,11418,12958,12965,12960,12963,12961,12962,12964,12927,13028,12957,11463,11461,11457,11460]
+        self.Measures['measure/measure-knee-circ-decr|incr'] = [11223,11230,11232,11233,11238,11228,11229,11226,11227,11224,11225,11221,11222,11239,11237,11236,13002,11235,11234,11223]
 
 
 
