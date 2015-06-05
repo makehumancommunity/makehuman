b0ac180c939fb3bcbe4877be3832a0e3cb13b138
Jonas Hauquier <duststorm@users.noreply.github.com>
2015-06-05 11:28:15 +0200

    Improve hg_get_release.sh and mention whether a changeset is in stable or unstable

--- buildscripts/hg_get_release.sh
+++ buildscripts/hg_get_release.sh
@@ -7,10 +7,26 @@
 # Usage: hg_get_release.sh changeset_hash
 ##
 
+function contains() { 
+    if [ "$(hg log -r $1 -b $2)" == "" ] ; 
+    then
+        # false
+        return 15
+    else
+        # true
+        return 0
+    fi; 
+}
+
+if contains stable $1 stable ; then
+    echo "stable"
+else
+    echo "unstable"
+fi
+
 TAGS=$( hg tags | awk '{ print $1 }' | tac )
 for TAG in $TAGS
 do
-    #echo $TAG
     if hg log -r "descendants($1) and $TAG" | grep -q changeset; then
         echo $TAG
         exit
