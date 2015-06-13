ec6aee53f094ffdaa7d85aec47d9a2ac94e82dc4
Jonas Hauquier <jonas.hauquier@makehuman.org>
2015-06-13 19:09:39 +0200

    Add extra event information when adding or removing proxies from human

--- makehuman/apps/human.py
+++ makehuman/apps/human.py
@@ -195,14 +195,20 @@ class Human(guicommon.Object, animation.AnimatedMesh):
         self._clothesProxies[uuid] = proxy
         event = events3d.HumanEvent(self, 'proxyChange')
         event.proxy = 'clothes'
+        event.action = 'add'
+        event.proxy_obj = proxy
         self.callEvent('onChanged', event)
 
     def removeClothesProxy(self, uuid):
         self._swapProxies(self._clothesProxies.get(uuid, None), None)
         event = events3d.HumanEvent(self, 'proxyChange')
+        proxy = None
         if uuid in self._clothesProxies:
+            proxy = self._clothesProxies[uuid]
             del self._clothesProxies[uuid]
         event.proxy = 'clothes'
+        event.action = 'remove'
+        event.proxy_obj = proxy
         self.callEvent('onChanged', event)
 
     def _swapProxies(self, oldPxy, newPxy):
