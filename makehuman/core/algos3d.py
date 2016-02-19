07807d5f6c94a20174ba3e68147cce57c6dc9768
Joel Palmius <joepal1976@hotmail.com>
2016-02-19 09:15:29 +0100

    Backed out changeset a3b6fdf789f9 (wrong branch)

--- makehuman/core/algos3d.py
+++ makehuman/core/algos3d.py
@@ -496,6 +496,6 @@ def defaultTargetLicense():
     license is globally referenced by and applies to the majority of targets.
     """
     import makehuman
-    return makehuman.getAssetLicense( {"license": "AGPL3",
+    return makehuman.getAssetLicense( {"license": "AGPL3 (http://www.makehuman.org/doc/node/makehuman_mesh_license.html)",
                                        "author": "Manuel Bastioni",
-                                       "copyright": "2016 Data Collection AB, Joel Palmius, Jonas Hauquier"} )
+                                       "copyright": "2014 Manuel Bastioni (mb@makehuman.org)"} )
