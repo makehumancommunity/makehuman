7079baa2b886b40b4dfc2826053f6797ab4d9e1d
Jonas Hauquier <duststorm@users.noreply.github.com>
2016-11-03 15:39:06 +0100

    Debugdump: report default and filesystem encoding

--- makehuman/lib/debugdump.py
+++ makehuman/lib/debugdump.py
@@ -96,6 +96,8 @@ class DebugDump(object):
         self.write("BASEMESH VERSION: %s", os.environ['MH_MESH_VERSION'])
         self.write("IS BUILT (FROZEN): %s", os.environ['MH_FROZEN'])
         self.write("IS RELEASE VERSION: %s", os.environ['MH_RELEASE'])
+        self.write("DEFAULT ENCODING: %s", sys.getdefaultencoding())
+        self.write("FILESYSTEM ENCODING: %s", sys.getfilesystemencoding())
         self.write("WORKING DIRECTORY: %s", getpath.pathToUnicode(os.getcwd()))
         self.write("HOME LOCATION: %s", getpath.pathToUnicode(getpath.getHomePath()))
         syspath = os.path.pathsep.join( [getpath.pathToUnicode(p) for p in sys.path] )
