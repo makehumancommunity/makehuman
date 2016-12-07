32b9409561b4112db2aa14baf7907d1ee76aff00
Yuri Pourre <yuripourre@gmail.com>
2016-12-07 12:23:17 -0200

    Add comments to bash script

--- buildscripts/rpm/install_rpm_dependencies.bash
+++ buildscripts/rpm/install_rpm_dependencies.bash
@@ -1,9 +1,12 @@
 #!/bin/sh 
+# Tries to use dnf
 installer="dnf"
 packages="numpy PyOpenGL PyQt4"
 
+# If dnf is not found, uses yum
 if ! command_exists="$(type -p "$installer")" || [ -z "$command_exists" ]; then
   installer="yum"
 fi
 
+# Install the packages
 $installer install $packages
