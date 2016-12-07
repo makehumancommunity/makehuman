3ff28b7b600d7bf57a9582feb7270f95b1c7685d
Yuri Pourre <yuripourre@gmail.com>
2016-12-07 11:59:42 -0200

    Update build script to use yum if dnf is missing

--- buildscripts/rpm/install_rpm_dependencies.bash
+++ buildscripts/rpm/install_rpm_dependencies.bash
@@ -1,5 +1,9 @@
-#!/bin/bash
+#!/bin/sh 
+installer="dnf"
+packages="numpy PyOpenGL PyQt4"
 
-dnf install numpy PyOpenGL PyQt4
+if command_exist="$(type -p "$installer")" || [ -z "$command_exist" ]; then
+  installer="yum"
+fi
 
-
+$installer install $packages
