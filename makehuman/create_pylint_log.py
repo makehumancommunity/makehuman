14bd8c3a44b6f74213450b3a631cfb4360c90bb0
Joel Palmius <joepal1976@hotmail.com>
2017-03-15 17:36:47 +0100

    Pylint support for the py3 branch

---
+++ makehuman/create_pylint_log.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+# On ubuntu you need "apt-get install pylint3" and the command is called
+# "pylint3". On other platforms you might want to change this.
+# 
+# I never tested if this even works on windows. 
+
+PYLINT="pylint3"
+
+import os
+
+pythonfiles = [];
+
+for dirName, subdirList, fileList in os.walk("."):
+    for fname in fileList:
+        filename, ext = os.path.splitext(fname)
+        if ext == ".py":
+            pythonfiles.append( os.path.join(dirName,fname) )
+
+os.system(PYLINT + " > pylint.log " + " ".join(pythonfiles))
+
