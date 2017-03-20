eefdc2cde1fdd0b0d15e4274dc526dad9393efb7
Aranuvir <Aranuvir@github>
2017-03-20 11:13:37 +0100

    port collada export to Pyhon3

--- makehuman/plugins/9_export_collada/mh2collada.py
+++ makehuman/plugins/9_export_collada/mh2collada.py
@@ -111,7 +111,7 @@ def exportCollada(filepath, config):
             fp = None
             log.error("Unable to open file for writing %s" % filepath)
 
-        date = time.strftime("%a, %d %b %Y %H:%M:%S +0000".encode('utf-8'), time.localtime()).decode('utf-8')
+        date = time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime())
         # TODO revise to make this enum-like
         if config.yUpFaceZ or config.yUpFaceX:
             upvector = "Y_UP"
