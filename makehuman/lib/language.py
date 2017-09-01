16417f9a3fb23e1b7d869871ddcec44682a25558
Aranuvir <Aranuvir@github>
2017-09-01 18:29:37 +0200

    fix IOError

--- makehuman/lib/language.py
+++ makehuman/lib/language.py
@@ -58,7 +58,7 @@ class Language(object):
         path = os.path.join(getSysDataPath("languages/"), lang + ".json")
         if not os.path.isfile(path):
             return
-        with io.open(path, 'rU') as f:
+        with io.open(path, 'r', encoding='utf-8') as f:
             try:
                 self.languageStrings = json.loads(f.read())
             except:
@@ -106,7 +106,7 @@ class Language(object):
         pathdir = os.path.dirname(path)
         if not os.path.isdir(pathdir):
             os.makedirs(pathdir)
-        with io.open(path, 'wb') as f:
+        with io.open(path, 'w', encoding='utf-8') as f:
             for string in self.missingStrings:
                 if self.language == "master":
                     f.write('"%s": "%s",\n' % (string.replace('\n', '\\n').encode('utf8'), string.replace('\n', '\\n').encode('utf8')))
