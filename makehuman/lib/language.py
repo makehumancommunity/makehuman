1210444404b669b648c9e53894bfbb8fe41600c9
Aranuvir <Aranuvir@github>
2017-09-01 19:02:51 +0200

    sort languages and if not mh.isRelease() add master

--- makehuman/lib/language.py
+++ makehuman/lib/language.py
@@ -188,7 +188,7 @@ def getLanguages():
         langDirFiles = langDirFiles + os.listdir(getDataPath('languages'))
     except:
         pass
-    return ['english'] + sorted([os.path.basename(filename).replace('.json', '') for filename in langDirFiles if filename.split(os.extsep)[-1] == "json"])
+    return ['english'] + [os.path.basename(filename).replace('.json', '') for filename in sorted(langDirFiles) if filename.split(os.extsep)[-1] == "json"]
 
 
 language = Language()
