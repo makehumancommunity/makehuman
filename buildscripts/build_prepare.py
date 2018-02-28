7ddc3b7812e108b131cb2486488f749a7b12a35c
Joel Palmius <joepal1976@hotmail.com>
2018-02-28 12:29:10 +0100

    Adapt build_prepare to use mhversion instead of manual hg, and force use of python3 if available

--- buildscripts/build_prepare.py
+++ buildscripts/build_prepare.py
@@ -6,11 +6,11 @@ MakeHuman build prepare
 
 **Project Name:**      MakeHuman
 
-**Product Home Page:** http://www.makehuman.org/
+**Product Home Page:** http://www.makehumancommunity.org/
 
 **Code Home Page:**    https://bitbucket.org/MakeHuman/makehuman/
 
-**Authors:**           Jonas Hauquier
+**Authors:**           Jonas Hauquier, Joel Palmius
 
 **Copyright(c):**      MakeHuman Team 2001-2018
 
@@ -88,6 +88,14 @@ import os
 import subprocess
 import shutil
 
+syspath = ["../makehuman","../makehuman/lib","../makehuman/apps","../makehuman/shared","../makehuman/core"]
+syspath.extend(sys.path)
+sys.path = syspath
+
+import getpath
+import gitutils
+import mhversion
+
 
 VERSION_FILE_PATH = 'makehuman/data/VERSION'
 BUILD_CONF_FILE_PATH = 'buildscripts/build.conf'
@@ -95,10 +103,9 @@ BUILD_CONF_FILE_PATH = 'buildscripts/build.conf'
 
 
 class MHAppExporter(object):
-    def __init__(self, sourceFolder, exportFolder='export', skipHG=False, skipScripts=False, noDownload=False):
+    def __init__(self, sourceFolder, exportFolder='export', skipScripts=False, noDownload=False):
         self.sourceFolder = sourceFolder
         self.targetFolder = exportFolder
-        self.skipHG = skipHG
         self.skipScripts = skipScripts
         self.noDownload = noDownload
 
@@ -120,36 +127,20 @@ class MHAppExporter(object):
 
         self.config = parseConfig(self.sourceFile(BUILD_CONF_FILE_PATH))
         if self.config is None:
-            print("No config file at %s, using defaults or options passed on commandline." % self.sourceFile(BUILD_CONF_FILE_PATH))
+            print("No config file at %s" % self.sourceFile(BUILD_CONF_FILE_PATH))
+            sys.exit(1)
         else:
             print("Using config file at %s. NOTE: properties in config file will override any other settings!" % self.sourceFile(BUILD_CONF_FILE_PATH))
 
-            global HG_PATH
             global GIT_PATH
 
-            HG_PATH = _conf_get(self.config, 'General', 'hgPath', HG_PATH)
             GIT_PATH = _conf_get(self.config, 'General', 'gitPath', GIT_PATH)
-
-            hgrev = _conf_get(self.config, 'BuildPrepare', 'hgRev', None)
-            if hgrev is not None:
-                self.HGREV, self.REVID = hgrev.split(':')
-            if self.HGREV and self.REVID:
-                os.environ['HGREVISION_SOURCE'] = "build config file"
-                os.environ['HGREVISION'] = self.HGREV
-                os.environ['HGNODEID'] = self.REVID
-            else:
-                self.HGREV = None
-                self.REVID = None
 
             self.VERSION_SUB = _conf_get(self.config, 'BuildPrepare', 'versionSub', None)
 
             isRelease = _conf_get(self.config, 'BuildPrepare', 'isRelease', None)
             if isRelease is not None:
                 self.IS_RELEASE = ( isRelease.lower() in ['yes', 'true'] )
-
-            skipHg = _conf_get(self.config, 'BuildPrepare', 'skipHg', None)
-            if skipHg is not None:
-                self.skipHG = skipHg.lower() in ['yes', 'true']
 
             skipScripts = _conf_get(self.config, 'BuildPrepare', 'skipScripts', None)
             if skipScripts is not None:
@@ -168,8 +159,8 @@ class MHAppExporter(object):
 
     def export(self):
         # Sanity checks
-        if not os.path.isdir(self.sourceFile('.hg')) and not os.path.isdir(self.sourceFile('.git')):
-            raise RuntimeError("The export folder %s is not found, the source folder argument should be the root of the hg or git repository." % self.sourceFile('.hg'))
+        if not os.path.isdir(self.sourceFile('.git')):
+            raise RuntimeError("The root source folder %s is not valid, the source folder argument should be the root of the git repository." % self.sourceFile('.git'))
         if self.isSubPath(self.targetFile(), self.sourceFile()):
             raise RuntimeError("The export folder is a subfolder of the source folder, this is not allowed.")
         if os.path.exists(self.targetFile()):
@@ -177,9 +168,7 @@ class MHAppExporter(object):
         if not os.path.exists(self.sourceFile()):
             raise RuntimeError("Specified source folder %s does not exist" % self.sourceFile())
         if not os.path.isfile(self.sourceFile('makehuman/makehuman.py')):
-            raise RuntimeError("The specified source folder %s does not appear to be a valid MakeHuman HG root folder (cannot find file %s)" % (self.sourceFile(), self.sourceFile('makehuman/makehuman.py')))
-
-        # TODO perform hg pull?
+            raise RuntimeError("The specified source folder %s does not appear to be a valid MakeHuman root folder (cannot find file %s)" % (self.sourceFile(), self.sourceFile('makehuman/makehuman.py')))
 
         print('Creating folder %s' % self.targetFile())
         os.makedirs(self.targetFile())
@@ -190,21 +179,11 @@ class MHAppExporter(object):
 
         print("\n\n%s build\n\n" % ("RELEASE" if self.isRelease() else "NIGHTLY"))
 
-        # Obtain exact revision
-        # TODO perhaps try to obtain hg tags instead of node id for releases
-        if not self.HGREV or not self.REVID:
-            self.HGREV, self.REVID = self.processRevision()
-        self.VERSION = self.getVersion()
-        print("Retrieved version information: %s (revision info: r%s %s [%s])\n" % (self.VERSION, self.HGREV, self.REVID, os.environ['HGREVISION_SOURCE']))
-
         # Run scripts to prepare assets
         if not self.skipScripts:
             self.runScripts()
 
-        if not os.path.isdir(self.sourceFile('.hg')):
-            self.exportGITFiles()
-        else:
-            self.exportHGFiles()
+        self.exportGITFiles()
 
         print("\n")
 
@@ -244,8 +223,8 @@ class MHAppExporter(object):
         print("\n")
 
         # Write VERSION file to export folder
-        print("Writing data/VERSION file to export folder with contents: %s:%s\n" % (self.HGREV, self.REVID))
-        self.writeVersionFile(self.HGREV, self.REVID)
+        print("Writing data/VERSION file to export folder\n")
+        self.writeVersionFile()
 
         # If RELEASE status or version-sub was set in config, update it in exported mh source file
         if (self.IS_RELEASE is not None) or (self.VERSION_SUB is not None) :
@@ -280,26 +259,10 @@ class MHAppExporter(object):
                 # Move folder in new root folder
                 print("Moving folder %s into new root %s" % (f, REARRANGE_ROOT_FOLDER))
                 shutil.move(path, self.targetFile(os.path.join(REARRANGE_ROOT_FOLDER, f)))
-        '''
-        # Move all contents of new root folder into dist root (overwrite if needed)
-        _fixOnFinish = []
-        for f in os.listdir( self.targetFile(REARRANGE_ROOT_FOLDER) ):
-            path = self.targetFile(os.path.join(REARRANGE_ROOT_FOLDER, f))
-            print ("Moving %s to root" % os.path.join(REARRANGE_ROOT_FOLDER, f))
-            if os.path.exists(self.targetFile(f)):
-                #print ("WARNING: overwriting folder of file %s with %s" % (f, os.path.join(REARRANGE_ROOT_FOLDER, f)))
-                _fixOnFinish.append(f)
-                shutil.move(path, self.targetFile(f+'--fixOnFinish'))
-            else:
-                shutil.move(path, self.targetFile(f))
-        # Remove the now empty root subfolder
-        os.rmdir(self.targetFile(REARRANGE_ROOT_FOLDER))
-        for f in _fixOnFinish:
-            shutil.move(self.targetFile(f+'--fixOnFinish'), self.targetFile(f))
-        '''
+
         print("\n")
 
-        resultInfo = ExportInfo(self.VERSION, self.HGREV, self.REVID, self.targetFile(), self.isRelease())
+        resultInfo = ExportInfo(self.VERSION, self.targetFile(), self.isRelease())
         resultInfo.rootSubpath = REARRANGE_ROOT_FOLDER
         resultInfo.datas = [os.path.join(REARRANGE_ROOT_FOLDER, d) for d in DATAS]
         resultInfo.pathEx = [os.path.join(REARRANGE_ROOT_FOLDER, p) for p in PYTHON_PATH_EX]
@@ -346,10 +309,17 @@ class MHAppExporter(object):
         return subprocess.check_call(args, cwd=self.getCWD())
 
     def runScripts(self):
+
+        pythonCmd = shutil.which("python3")
+        if pythonCmd is None:
+            pythonCmd = shutil.which("python")
+        if pythonCmd is None:
+            pythonCmd = "python"
+
         if not self.noDownload:
             ###DOWNLOAD ASSETS
             try:
-                self.runProcess( ["python","download_assets_git.py"] )
+                self.runProcess( [pythonCmd,"download_assets_git.py"] )
             except subprocess.CalledProcessError:
                 print("check that download_assets_git.py is working correctly")
                 sys.exit(1)
@@ -357,7 +327,7 @@ class MHAppExporter(object):
 
         ###COMPILE TARGETS
         try:
-            self.runProcess( ["python","compile_targets.py"] )
+            self.runProcess( [pythonCmd,"compile_targets.py"] )
         except subprocess.CalledProcessError:
             print("check that compile_targets.py is working correctly")
             sys.exit(1)
@@ -365,7 +335,7 @@ class MHAppExporter(object):
 
         ###COMPILE MODELS
         try:
-            self.runProcess( ["python","compile_models.py"] )
+            self.runProcess( [pythonCmd,"compile_models.py"] )
         except subprocess.CalledProcessError:
             print("check that compile_models.py is working correctly")
             sys.exit(1)
@@ -373,7 +343,7 @@ class MHAppExporter(object):
 
         ###COMPILE PROXIES
         try:
-            self.runProcess( ["python","compile_proxies.py"] )
+            self.runProcess( [pythonCmd,"compile_proxies.py"] )
         except subprocess.CalledProcessError:
             print("check that compile_proxies.py is working correctly")
             sys.exit(1)
@@ -384,28 +354,6 @@ class MHAppExporter(object):
             return EXCLUDES + EXCLUDES_RELEASE
         else:
             return EXCLUDES
-
-    def exportHGFiles(self):
-        print("Exporting files from hg repo (hg archive)")
-        excludes = self.getExcludes()
-        exclarg = [item for pair in zip(len(excludes)*['--exclude'], excludes) for item in pair]
-
-        try:
-            self.runProcess( [HG_PATH, "archive"] + exclarg + [self.targetFile()])
-        except Exception as e:
-            print("An error happened attempting to run 'hg archive'. Is Mercurial (commandline-tools) installed?")
-            raise e
-
-        # Because the --excludes option does not appear to be working all too well (at least not with wildcards):
-        # Gather files
-        files = []
-        _recursive_glob(self.targetFile(), self.getExcludes(), files)
-        for f in files:
-            print("Removing excluded file from export folder %s" % f)
-            if os.path.isdir(self.targetFile(f)):
-                shutil.rmtree(self.targetFile(f))
-            else:
-                os.remove(self.targetFile(f))
 
     def exportGITFiles(self):
         print("Exporting files from git repo")
@@ -443,54 +391,23 @@ class MHAppExporter(object):
             print("Copying %s" % self.sourceFile(f))
             shutil.copy(self.sourceFile(f), self.targetFile(f))
 
-    def processRevision(self):
-        """
-        Find revision, if source control is to be skipped, attempts to read it
-        from data/VERSION which should be already present.
-        """
-        if self.skipHG:
-            try:
-                vfile = open(self.sourceFile(VERSION_FILE_PATH),"r")
-                rIn = vfile.read().split(':')
-                HGREV = int(rIn[0])
-                REVID = rIn[1].strip()
-                vfile.close()
-            except:
-                print('Warning: no VERSION file found, HG revision unknown')
-                HGREV = '?'
-                REVID = 'UNKNOWN'
-
-            # Store rev to prevent repeated revision queries in makehuman.py
-            os.environ['HGREVISION_SOURCE'] = "data/VERSION static revision data"
-            os.environ['HGREVISION'] = HGREV
-            os.environ['HGNODEID'] = REVID
-        else:
-            rev = self.getRevision()
-            if rev is None:
-                HGREV = '?'
-                REVID = 'UNKNOWN'
-                return HGREV, REVID
-            else:
-                HGREV = rev[0]
-                REVID = rev[1]
-
-        return HGREV, REVID
-
-    def writeVersionFile(self, hgrev, revid):
+    def writeVersionFile(self):
         ### Write VERSION file
         versionFile = self.targetFile(VERSION_FILE_PATH)
         if not os.path.isdir(os.path.dirname( versionFile )):
             os.makedirs(os.path.dirname( versionFile ))
-        vfile = open(versionFile, "w")
-        vfile.write("%s:%s" % (hgrev, revid))
-        vfile.close()
 
+        mhv = mhversion.MHVersion()
+        mhv.writeVersionFile(versionFile)
 
 class ExportInfo(object):
-    def __init__(self, version, revision, nodeid, path, isRelease):
+    def __init__(self, version, path, isRelease):
+
+        mhv = mhversion.MHVersion()
+
         self.version = version
-        self.revision = revision
-        self.nodeid = nodeid
+        self.branch = mhv.currentBranch
+        self.commit = mhv.currentShortCommit
         self.path = os.path.abspath(path)
         self.isRelease = isRelease
         self.datas = []
@@ -540,9 +457,9 @@ class ExportInfo(object):
     def __str__(self):
         return """MH source export
   version:  %s
-  revision: %s (%s)
+  commit:   %s (%s)
   path:     %s
-  type:     %s""" % (self.version, self.revision, self.nodeid, self.path, "RELEASE" if self.isRelease else "NIGHTLY")
+  type:     %s""" % (self.version, self.commit, self.branch, self.path, "RELEASE" if self.isRelease else "NIGHTLY")
 
 def _recursive_glob(p, matchPatterns, files, root = None):
     import os
@@ -590,7 +507,7 @@ def parseConfig(configPath):
     else:
         return None
 
-def export(sourcePath = '.', exportFolder = 'export', skipHG = False, skipScripts = False, noDownload = False):
+def export(sourcePath = '.', exportFolder = 'export', skipScripts = False, noDownload = False):
     """
     Perform export
 
@@ -598,12 +515,11 @@ def export(sourcePath = '.', exportFolder = 'export', skipHG = False, skipScript
     exportFolder    The folder to export to. Should not exist before running.
     skipScripts     Set to true to skip executing asset compile scripts
     noDownload      Skip downloading files (has no effect when skipScripts is already enabled)
-    skipHG          Set to true to skip verifying HG revision status (tries to read from data/VERSION)
 
     Returns a summary object with members: version, revision, path (exported path), isRelease
     containing information on the exported release.
     """
-    exporter = MHAppExporter(sourcePath, exportFolder, skipHG, skipScripts, noDownload)
+    exporter = MHAppExporter(sourcePath, exportFolder, skipScripts, noDownload)
     return exporter.export()
 
 
@@ -617,11 +533,10 @@ def _parse_args():
     # optional arguments
     parser.add_argument("--skipscripts", action="store_true", help="Skip running scripts for compiling assets")
     parser.add_argument("--nodownload", action="store_true", help="Do not run download assets script")
-    parser.add_argument("--skiphg", action="store_true", help="Skip retrieving HG revision")
 
     # positional arguments
-    parser.add_argument("sourcePath", default=None, nargs='?', help="Root path of HG source repository")
-    parser.add_argument("targetPath", default=None, nargs='?', help="Path to export to")
+    parser.add_argument("sourcePath", default=None, nargs='?', help="Root path of makehuman source directory")
+    parser.add_argument("targetPath", default=getpath.getPath("build-prepare-export"), nargs='?', help="Path to export to")
 
     argOptions = vars(parser.parse_args())
     return argOptions
@@ -634,4 +549,4 @@ if __name__ == '__main__':
     if args.get('targetPath', None) is None:
         raise RuntimeError("targetPath argument not specified")
 
-    print(export(args['sourcePath'], args['targetPath'], args.get('skiphg', False), args.get('skipscripts', False), args.get('nodownload', False)))
+    print(export(args['sourcePath'], args['targetPath'], args.get('skipscripts', False), args.get('nodownload', False)))
