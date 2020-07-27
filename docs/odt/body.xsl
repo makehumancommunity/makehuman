ea11dae75b80dab6b71b728ab160d0ad466b18b4
Joel Palmius <joepal1976@hotmail.com>
2020-07-27 08:48:55 +0200

    Deprecate the docs directory as it hasn't been updated in five years

--- docs/odt/body.xsl
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="utf-8" ?>
-<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:fo="http://www.w3.org/1999/XSL/Format">
-
-  <xsl:template match="Body">
-    <xsl:apply-templates />
-  </xsl:template>
-
-</xsl:stylesheet>
-
--- docs/odt/syncimages.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p rst/images
-mkdir -p rst/_images
-
-rsync -av --delete makehuman@www.makehumancommunity.org:www.makehumancommunity.org/files/images/ rst/images/
-rsync -av --delete makehuman@www.makehumancommunity.org:www.makehumancommunity.org/root/images/ rst/_images/
-
-
